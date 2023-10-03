c3e7777d44ef8a9e941ceeb3e5f3ba48dee20c81
HAIZAKURA <hhbilly99@gmail.com>
2023-10-03 15:22:33 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const axios = require('axios')
-const bodyParser = require('body-parser')
-const express = require('express')
-const log4js = require('log4js')
+// app.js
+// Service Application
 
-const app = express()
-const port = 3010
-const logger = log4js.getLogger()
-logger.level = 'info'
+import bodyParser from 'body-parser';
+import express from 'express';
+import log4js from 'log4js';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 3010;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const logger = log4js.getLogger();
+logger.level = 'info';
 
 app.use(log4js.connectLogger(
     logger, 
@@ -14,62 +19,47 @@ app.use(log4js.connectLogger(
         level: 'auto',
         format: ':remote-addr ":method :url" :status ":user-agent"' 
     }
-))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST");
     next();
-})
+});
+app.use(express.static(join(__dirname, 'public')));
+
+export { logger };
+
+/** Router Begin **/
+
+/**
+ * Code Define
+ * 
+ * 200: OK
+ * 400: Bad Request
+ * 502: Bad Gateway
+ */
+
+/** Rate Router **/
+import { rate_router } from './router/rate.js';
+app.use('/api/rate', rate_router);
+
+/** Router End **/
 
 /**
  * robots.txt
  */
 app.get('/robots.txt', (req, res) => {
-    res.type('text/plain')
-    res.send("User-agent: *\nDisallow: /")
-    return
-})
-
-/**
- * get rate
- * request.query = year, month, day, transCur, baseCur
- * response.body = { 'code': code, 'data': '' }
- */
-app.get('/api/rate', (req, res) => {
-    let year = req.query.year
-    let month = req.query.month
-    let day = req.query.day
-    let transCur = req.query.transCur
-    let baseCur = req.query.baseCur
-    if (!year && !month && !day) {
-        let today = new Date();
-        year = today.getFullYear();
-        month = today.getMonth() + 1;
-        month = month < 10 ? '0' + month : month;
-        day = today.getDate();
-        day = day < 10 ? '0' + day : day;
-    }
-    axios.get('https://www.unionpayintl.com/upload/jfimg/' + year + month + day + '.json')
-    .then(res => {
-        let exchangeRateJson = res.data.exchangeRateJson;
-        exchangeRateJson.forEach(element => {
-            if (element.transCur == transCur & element.baseCur == baseCur) {
-                console.log(element);
-                return;
-            }
-        });
-    })
-    .catch(err => {
-        console.log(err);
-    })
-})
+    res.type('text/plain');
+    res.send("User-agent: *\nDisallow: /");
+    return;
+});
 
 /**
  * service start
  */
 app.listen(port, () => {
-    logger.info('Service Starting...')
-    logger.info(`The Service is listening on port ${port}.`)
-})
+    logger.info('Service Starting...');
+    logger.info(`The Service is listening on port ${port}.`);
+});
